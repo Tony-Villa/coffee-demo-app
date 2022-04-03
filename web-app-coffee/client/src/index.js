381ada00c422a8f9aecc9d9a1c5aef795934bea3
Tony Villa <tonyvilla77@gmail.com>
2022-04-02 21:06:41 -0700

    added frontend dependencies

--- web-app-coffee/client/src/index.js
+++ web-app-coffee/client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
